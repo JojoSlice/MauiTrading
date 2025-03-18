a5ca6631944485cc2703829ecb327285bf422fec
Jojo Lindell <johannes152@hotmail.com>
2025-03-18 17:23:48 +0100

    Quick Update

--- MauiTrading/Models/PnL.cs
+++ MauiTrading/Models/PnL.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MauiTrading.Models
 {
     public class PnLData
     {
-        public string UserId { get; set; } = string.Empty;
-        public string SeasonId { get; set; } = string.Empty;
-        public double PnL { get; set; } = 0;
-        public DateTime Date { get; set; } = DateTime.UtcNow;
+        [JsonPropertyName("id")]
+        public string id { get; set; }
+        [JsonPropertyName("userId")]
+        public string UserId { get; set; }
+        [JsonPropertyName("seasonId")]
+        public string SeasonId { get; set; }
+        [JsonPropertyName("pnL")]
+        public double PnL { get; set; }
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
     }
 }
